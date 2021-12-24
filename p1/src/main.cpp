f684a4170c5f8d09b28d445b3f5f7f2a0afa9064
alravid <allancravid@tecnico.ulisboa.pt>
2021-12-24 10:28:38 +0000

    add initial template ?

---
+++ p1/src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+
+
+class Solution1
+{
+//public:
+  //MyClass(int t_data)
+  //  : m_data(t_data)
+  //{
+
+  //}
+
+  //int getData() const
+  //{
+  // return m_data;
+  //}
+
+//private:
+};
+
+
+class Solution2
+{
+//public:
+  //MyClass(int t_data)
+  //  : m_data(t_data)
+  //{
+
+  //}
+
+  //int getData() const
+  //{
+  // return m_data;
+  //}
+
+//private:
+};
+
+int main()
+{
+
+
+
+
+
+}
