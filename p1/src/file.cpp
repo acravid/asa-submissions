0d45a4714c486c3116557451738a45584d64cd84
alravid <allancravid@tecnico.ulisboa.pt>
2022-01-05 14:55:26 +0000

    add problem 2 I/O

--- p1/src/file.cpp
+++ p1/src/file.cpp
@@ -2,27 +2,55 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <algorithm>
+#include <unordered_map>
+//#include <algorithm>
 
 #define P1 1
 #define P2 2
 #define newline "\n"
 
 
-void lic_p1(std::vector<int> nums) {
-  int n = nums.size();
+
+
+void process_input(std::vector<int> &a, std::vector<int> &b, int lines) {
+  
+  std::unordered_map<int,int> commom_aux;
+  int i = 0;
+  while(i < lines) {
+    std::string line;
+    int input;
+    getline(std::cin, line);
+    std::istringstream iss(line);
+    while( iss >> input){
+      if(i == 0) {
+        a.push_back(input);
+        commom_aux.insert(std::make_pair(input,input));
+      } else {
+        if((commom_aux.count(input) != 0)) {
+            b.push_back(input);
+        }
+      }
+    }
+    i++;
+  }
+}
+
+
+void lic_p1(std::vector<int> numbers) {
+  int n = numbers.size();
   
   if(n < 1) { // base case
     std::cout <<  n << " " << n << newline;
     return;
   }
-
+                                                       // stores the count of all LIS that ends at numbers[i]             
   std::vector<int> longest_subsequence_at_index(n,1),  longest_subsequence_counter_at_index(n,1);
   int longest_subsequence = 1, previous_longest, new_longest; 
 
+  // O(N^2) 
   for(int i = 1; i < n; i++) {
     for(int j = 0; j < i; j++) {
-      if(nums[i] > nums[j]) {                                            
+      if(numbers[i] > numbers[j]) {                                            
 
         previous_longest = longest_subsequence_at_index[i];
         new_longest = longest_subsequence_at_index[j] + 1;
@@ -59,15 +87,10 @@ void lic_p1(std::vector<int> nums) {
 }
 
 
-void lcis_p2() {
 
 
 
-
-}
-
- int main()
-{
+int main() {
     int command;
 
     std::cin >> command;
@@ -78,7 +101,6 @@ void lcis_p2() {
   
       std::string line_to_process;
       std::vector<int> numbers;
-  
       // pushes values to the vector 
       while(getline(std::cin, line_to_process, ' ')) {
         numbers.push_back(std::stoi(line_to_process));
@@ -88,8 +110,11 @@ void lcis_p2() {
 
     } else if(command == P2) {
 
-      std::cout << "Problema 2" << newline;
-      // do somehing
+      std::vector<int> first_vector;
+      std::vector<int> second_vector;
+
+      process_input(first_vector, second_vector, command);
+      lcis_p2(first_vector,second_vector); 
     } else {
       std::cout << "An error has occurred, try again with different values" << newline;
     }
